d5e921d4ea55817ea94e6d370580bd4981289ffb
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2022-03-14 12:39:44 +0200

    add iterative solution

--- random-tasks/is-linked-list-perm.cpp
+++ random-tasks/is-linked-list-perm.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stack>
 
 struct linkedList {
 	int data;
@@ -7,6 +8,8 @@ struct linkedList {
 	linkedList(int d, linkedList* n = nullptr) : data(d), next(n) {}
 };
 
+// Solution inspired by cppnuts: 
+// https://www.youtube.com/watch?v=s6GAAml3Z0M&t
 bool checkIfPalindromeRecursive(const linkedList*& left, const linkedList* rigth) {
 	if (rigth == nullptr)
 		return true;
@@ -21,6 +24,32 @@ bool checkIfPalindrome(const linkedList* l) {
 	return checkIfPalindromeRecursive(l, l);
 }
 
+// my iterative approach
+bool checkIfPalindromeIterative(const linkedList* l) {
+	std::stack<const linkedList*> s;
+
+	s.push(l);
+	int size = 0;
+
+	while (s.top()) {
+		s.push(s.top()->next); 
+		++size;
+	}
+
+	int mid = size / 2;
+	s.pop();
+	for (int i = 0; i < mid; i++) {
+		if (l->data != s.top()->data)
+			return false;
+
+		s.pop();
+		l = l->next;
+	}
+
+	return true;
+
+}
+
 void freeLinkedList(linkedList* toDel) {
 	if (toDel == nullptr)
 		return;
@@ -33,10 +62,12 @@ int main() {
 	linkedList* pal = new linkedList(1,
 		new linkedList(2,
 			new linkedList(3,
-				new linkedList(2,
-					new linkedList(1)))));
+				new linkedList(4,
+					new linkedList(3, 
+						new linkedList(2,
+							new linkedList (1)))))));
 
-	std::cout << checkIfPalindrome(pal);
+	std::cout << checkIfPalindromeIterative(pal);
 
 	freeLinkedList(pal);
 }
