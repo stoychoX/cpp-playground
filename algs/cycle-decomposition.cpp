fdd16ccd4ac4edf4683549d7776778db6efcaf7c
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2022-05-20 01:44:48 +0300

    Create cycle-decomposition.cpp

---
+++ algs/cycle-decomposition.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+using matrix = std::vector<std::vector<int>>;
+
+matrix findCycleDecomposition(const std::vector<int>& permutation) {
+	matrix toReturn;
+	const int length = permutation.size();
+
+	std::vector<bool> visited(length, false);
+  
+  // the if-nesting on row 14 could be avoided.
+	for (size_t i = 0; i < length; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			if (permutation[i] != i + 1) {
+				toReturn.push_back(std::vector<int>(1, i + 1));
+				const unsigned idx = toReturn.size() - 1;
+				int k = permutation[i];
+
+				while (k != i + 1) {
+					visited[k - 1] = true;
+					toReturn[idx].push_back(k);
+					k = permutation[k - 1];
+				}
+			}
+		}
+	}
+
+	return toReturn;
+}
+
+int main() {
+	std::vector<int> permutation = { 3, 4, 5, 6, 1, 8, 7, 2, 9 };
+
+	matrix result = findCycleDecomposition(permutation);
+
+	for (size_t i = 0; i < result.size(); i++) {
+		for (size_t j = 0; j < result[i].size(); j++) {
+			std::cout << result[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
