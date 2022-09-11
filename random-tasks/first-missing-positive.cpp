80bf91d09d8441193b4dfc5d8a9e1b14caefe85a
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2022-09-11 23:04:33 +0300

    Create first-missing-positive.cpp

---
+++ random-tasks/first-missing-positive.cpp
@@ -0,0 +1,42 @@
+// leetcode task to find the least positive number not contained in array
+#include<utility>
+#include<vector>
+#include<iostream>
+
+class Solution {
+public:
+    int firstMissingPositive(std::vector<int> nums) {
+        int result = INT_MAX;
+        bool hasZero = false;
+
+        for (size_t i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                hasZero = true;
+                break;
+            }
+        }
+
+
+        for (int i = 0; i < nums.size(); i++) {
+            while (nums[i] > 0 && nums[i] < nums.size() + !hasZero && nums[i] != i + !hasZero && nums[i] != nums[nums[i] - !hasZero]) {
+                std::swap(nums[i], nums[nums[i] - !hasZero]);
+            }
+        }
+
+        for (int i = hasZero; i < nums.size(); i++) {
+            if (nums[i] != i + !hasZero) {
+                result = i;
+                break;
+            }
+        }
+
+        if (result == INT_MAX)
+            return nums.size() + !hasZero;
+        return result + !hasZero;
+    }
+};
+
+int main() {
+    Solution s;
+    std::cout <<  s.firstMissingPositive({ 0, -1, 3, 1 });
+}
