a24dc39705ffb7f5dc17f6cb1770cb982240e00b
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2023-04-09 15:51:43 +0300

    even better solution

--- random-tasks/lengthOfLongestSubstring.cpp
+++ random-tasks/lengthOfLongestSubstring.cpp
@@ -7,19 +7,12 @@ int lengthOfLongestSubstring(const std::string& s) {
         size_t pos = 0;
         size_t ans = 0;
 
-        bool metDuplicate = false;
-
         for (size_t i = 0; i < s.length(); i++) {
             bool contains = !(lastPos.count(s[i]) == 0);
 
             size_t lastPosOfChar = lastPos[s[i]];
 
             if (contains && lastPosOfChar >= pos) {
-                if (!metDuplicate) {
-                    ans = i;
-                    metDuplicate = true;
-                }
-
                 ans = std::max(ans, i - pos);
                 pos = lastPosOfChar + 1;
             }
@@ -30,12 +23,8 @@ int lengthOfLongestSubstring(const std::string& s) {
 
         ans = std::max(ans, s.length() - pos);
 
-        if (metDuplicate == false) {
-            return s.length();
-        }
-
         return ans;
-    }
+}
 
 int main() {
     std::cout << lengthOfLongestSubstring("dvdf");
